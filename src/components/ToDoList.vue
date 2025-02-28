fdf6a7cb17cd9bacc036b01d816d78ecb0f67fab
Mykhailo Chynkov <mykhailo.chynkov@gmail.com>
2025-02-28 21:47:38 +0200

    Complete button. List of completed todos.

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,30 +1,36 @@
 <script setup>
 import { ref } from 'vue'
 
-const todos = ref([])
+const todosInProgress = ref([])
+const todosCompleted = ref([])
 const newToDo = ref('')
 const ToDo_Index_ForEditing = ref(0) //Do I really need this?
 const ToDo_Text_ForEditing = ref('')
 const isVisible = ref(true)
 
 function addToDo() {
-  todos.value.push(newToDo.value)
+  todosInProgress.value.push(newToDo.value)
   newToDo.value = ''
 }
 
 function removeToDo(index) {
-  todos.value.splice(index, 1)
+  todosInProgress.value.splice(index, 1)
 }
 
 function editeToDo(index) {
   isVisible.value = !isVisible.value
   ToDo_Index_ForEditing.value = index
-  ToDo_Text_ForEditing.value = todos.value.at(ToDo_Index_ForEditing.value)
+  ToDo_Text_ForEditing.value = todosInProgress.value.at(ToDo_Index_ForEditing.value)
 }
 
 function saveToDo() {
-  todos.value[ToDo_Index_ForEditing.value] = ToDo_Text_ForEditing.value
+  todosInProgress.value[ToDo_Index_ForEditing.value] = ToDo_Text_ForEditing.value
   isVisible.value = !isVisible.value
+}
+
+function completeToDo(index) {
+  todosCompleted.value.push(todosInProgress.value[index])
+  removeToDo(index)
 }
 </script>
 
@@ -35,11 +41,19 @@ function saveToDo() {
       <button @click="addToDo">Add ToDo</button>
     </div>
     <div>
+      <h1>In Progress:</h1>
       <ul>
-        <li v-for="(todo, index) in todos" :key="index">
+        <li v-for="(todo, index) in todosInProgress" :key="index">
           {{ todo }}
           <button @click="removeToDo(index)">Delete</button>
           <button @click="editeToDo(index)">Edite</button>
+          <button @click="completeToDo(index)">Complete</button>
+        </li>
+      </ul>
+      <h1>Completed:</h1>
+      <ul>
+        <li v-for="(todo, index) in todosCompleted" :key="index">
+          {{ todo }}
         </li>
       </ul>
     </div>
